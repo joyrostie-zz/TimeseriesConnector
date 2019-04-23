885d28f513531817357ce5e87a7b8bed772539a5
Valentyn Dembitskyi <valentyn.dembitskyi@itera.no>
2019-04-23 12:19:32 +0300

    syntax update

---
+++ src/components/TimeseriesConnector/TimeseriesConnector.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { TenantSelector } from "@cognite/gearbox";
+import { ReactAuthProvider } from "@cognite/react-auth";
+import TimeseriesContainer from '../../containers/TimeseriesContainer/TimeseriesContainer'
+import styled from 'styled-components';
+import 'antd/dist/antd.css';
+
+const PageContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TenantSelectorContainer = styled.div`
+  max-width: 600px;
+  min-width: 400px;
+`;
+
+class TimeseriesConnector extends React.Component {
+  state = {
+    tenant: null
+  };
+
+  handleTenantSelect = tenant => {
+    this.setState({
+      tenant
+    });
+    window.localStorage.setItem('tenant', JSON.stringify(tenant))
+  }
+
+  componentWillMount() {
+    if (window.localStorage.length) {
+      let tenant = JSON.parse(window.localStorage.getItem('tenant'));
+      this.setState({
+        tenant
+      })
+      window.localStorage.removeItem('tenant')
+    }
+  }
+
+  render() {
+    const { tenant } = this.state;
+
+    return (
+      <PageContainer>
+        {tenant ? (
+          <ReactAuthProvider
+            project={this.state.tenant}
+            redirectUrl={window.location.href}
+            errorRedirectUrl={window.location.href}
+          >
+            <TimeseriesContainer />
+          </ReactAuthProvider>
+        ) : (
+            <TenantSelectorContainer>
+              <TenantSelector
+                onTenantSelected={this.handleTenantSelect}
+                initialTenant='itera-dev'
+                title='TimeseriesConnector'
+              />
+            </TenantSelectorContainer>
+          )}
+      </PageContainer>
+    );
+  }
+}
+
+export default TimeseriesConnector;
---
+++ src/containers/TimeseriesContainer/TimeseriesContainer.js
@@ -0,0 +1,137 @@
+import React from 'react';
+import { AssetTree, TimeseriesSearch } from "@cognite/gearbox";
+import * as sdk from '@cognite/sdk';
+import styled from 'styled-components';
+import { Button, message, notification } from 'antd';
+
+const ButtonGroup = Button.Group;
+
+const PageContainer = styled.div`
+    width: 90vw;
+    height: 80vh;
+    display: flex;
+    justify-content: left;
+`;
+
+const PageTree = styled.div`
+    width: 40%;
+`;
+
+const PageSeries = styled.div`
+    width: 45%;
+`;
+
+const PageButtons = styled.div`
+    width: 15%;
+    padding: 0 0 0 5px;
+`;
+
+class TimeseriesContainer extends React.Component {
+    state = {
+        assetsFetched: [],
+        assetChecked: null,
+        timeseriesChecked: [],
+    }
+
+    componentDidMount() {
+        sdk.Assets.list()
+            .then(data => {
+                this.setState({
+                    assetsFetched: data.items
+                });
+            });
+    }
+
+    handleAssetSelect = (data) => {
+        //User actions feedback info
+        console.log(`Asset checked: ${data.title}`);
+
+        this.setState({
+            assetChecked: data.key,
+        });
+    }
+
+    handleSeriesSelect = (ids) => {
+        this.setState({
+            timeseriesChecked: ids,
+        });
+
+    }
+
+    handleSeriesConnect = async () => {
+        const timeseriesIds = this.state.timeseriesChecked.slice();
+
+        const changes = timeseriesIds.map(elem => {
+            return { id: elem, assetId: { set: this.state.assetChecked } }
+        })
+
+        if (changes.length) {
+            await sdk.TimeSeries.updateMultiple(changes);
+
+            //User actions feedback info
+            const seriesString = timeseriesIds.join(', ')
+            // message.info(`Asset: ${this.state.assetChecked} was connected to TimeSeries: ${seriesString}`)
+            notification.open({
+                message: 'Successfully connected',
+                description: `Asset: ${this.state.assetChecked} was connected to TimeSeries: ${seriesString}`,
+                duration: 3
+            })
+            console.log(`Asset: ${this.state.assetChecked} was connected to TimeSeries: ${seriesString}`);
+        } else {
+            //User actions feedback info
+            message.info('Nothing to connect');
+            console.log('Nothing to connect');
+        }
+    }
+
+    handleSeriesDisconnect = async () => {
+        const allSeries = (await sdk.TimeSeries.list()).items;
+        const connectedSeries = allSeries.filter(elem => elem.assetId);
+
+        const changes = connectedSeries.map(elem => {
+           return { id: elem.id, assetId: { setNull: true } }
+        })
+
+        if (changes.length) {
+            await sdk.TimeSeries.updateMultiple(changes);
+
+            //User actions feedback info
+            message.info("All TimeSeries were disconnected");
+            console.log("All TimeSeries were disconnected");
+        }   else {
+            //User actions feedback info
+            message.info('Nothing to disconnect');
+            console.log('Nothing to disconnect');
+        }
+    }
+
+    render() {
+        const { assetsFetched } = this.state;
+        const filterRule = (timeseries) => !timeseries.assetId;
+
+        return (
+            <PageContainer>
+                <PageTree>
+                    {assetsFetched.length ? <AssetTree
+                        assets={assetsFetched}
+                        onSelect={this.handleAssetSelect}
+                         /> : null }
+                </PageTree>
+                <PageSeries>
+                     <TimeseriesSearch
+                        onTimeserieSelectionChange={this.handleSeriesSelect}
+                        filterRule={filterRule}
+                     />
+                </PageSeries>
+                <PageButtons>
+                    <ButtonGroup>
+                        <Button type="primary" onClick={this.handleSeriesConnect}>Connect</Button>
+                        <Button type="primary" onClick={this.handleSeriesDisconnect}>Disconnect all</Button>
+                    </ButtonGroup>
+                </PageButtons>
+            </PageContainer>
+        );
+    }
+}
+
+export default TimeseriesContainer;
